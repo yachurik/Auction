47a7ed198d2d9d786fc91ebd58d932d2025fc8fc
yachurik <churik0509@mail.ru>
2024-06-22 13:33:40 +0300

    1.1

--- bot.py
+++ bot.py
@@ -53,7 +53,36 @@ def handle_start(message):
 Для этого нужно быстрее всех нажать на кнопку 'Получить!'
 
 Только три первых пользователя получат картинку!)""")
-        
+
+
+@bot.message_handler(commands=['rating'])
+def handle_rating(message):
+    res = manager.get_rating() 
+    res = [f'| @{x[0]:<11} | {x[1]:<11}|\n{"_"*26}' for x in res]
+    res = '\n'.join(res)
+    res = f'|USER_NAME    |COUNT_PRIZE|\n{"_"*26}\n' + res
+    bot.send_message(message.chat.id, res)
+    
+    
+    
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+
+    prize_id = call.data
+    user_id = call.message.chat.id
+
+    if manager.get_winners_count(prize_id) < 3:
+        res = manager.add_winner(user_id,prize_id)
+        if res:
+            img = manager.get_prize_img(prize_id)
+            with open(f'img/{img}', 'rb') as photo:
+                bot.send_photo(user_id, photo, caption="Поздравляем! Ты получил картинку!")
+        else:
+            bot.send_message(user_id, 'Ты уже получил картинку!')
+    else:
+        bot.send_message(user_id, "К сожалению, ты не успел получить картинку! Попробуй в следующий раз!)")
+
+ 
 
 
 def polling_thread():
--- logic.py
+++ logic.py
@@ -82,16 +82,40 @@ class DatabaseManager:
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute('SELECT image FROM prize_id')
+            cur.execute('SELECT image FROM prizes WHERE prize_id = ?', (prize_id))
         return cur.fetchall()[0][0]
 
     def get_random_prize(self):
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute('')
+            cur.execute('SELECT * FROM prizes WHERE used = 0 ORDER BY RANDOM() LIMIT 1  ')
         return cur.fetchall()[0]
     
+
+
+    def get_winners_count(self, prize_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT COUNT(*) FROM winners WHERE prize_id = ? ', (prize_id, ))
+            return cur.fetchall()[0][0]
+        
+    def get_rating(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('''
+    SELECT users.user_name , 
+    COUNT(winners.prize_id) as count_prize ,
+    FROM users
+    INNER JOIN users on users.user_id = winners.user_id
+    GROUP BY winners.user_id
+    ORDER BY count_prize                   
+    LIMIT 10
+    ''')
+            return cur.fetchall()
+
   
 def hide_img(img_name):
     image = cv2.imread(f'img/{img_name}')
